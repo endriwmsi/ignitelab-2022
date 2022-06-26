15bc77a224b2eec9dead9cb6ebfa645934b1a3c1
Endriw Schiavenato <100975346+endriwschiavenato@users.noreply.github.com>
2022-06-26 11:48:25 -0300

    Delete ignite/event-platform directory

--- ignite/event-platform/src/lib/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4u2too70i9i01t7bcj5afhn/master',
-    cache: new InMemoryCache()
-})
