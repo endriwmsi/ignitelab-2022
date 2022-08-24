a138b503e1f4512a8e4e4d40355ca41916528d13
Endriw Schiavenato <endriwm.schiavenato@rede.ulbra.br>
2022-08-24 10:37:20 -0300

    Correção de irregularidade que não permitia logar uma segunda vez.

--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -10,7 +10,7 @@ export function Event() {
       <Header />
       <main className="flex flex-1">
         { slug 
-          ? <Video lessonSlug={slug}/> 
+          ? <Video lessonSlug={slug} /> 
           : <div className="flex-1" /> 
         }
         <Sidebar />
